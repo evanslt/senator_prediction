ce3ca9ec51a31437132d8495f29cbc21de1a1ef3
Lenny Evans <levans@uber.com>
2017-06-30 08:40:52 -0400

    initial commit

---
+++ faces.py
@@ -0,0 +1,106 @@
+import numpy as np
+import sys
+import os
+
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import Activation
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from PIL import Image
+
+def fit_generator(files, batch_size, input_shape,print_stats=True,
+                  one_epoch=False, epoch_sizes=[]):
+
+    X = np.zeros((batch_size,) + input_shape)
+    Y = np.zeros(batch_size)
+
+    while True:
+        np.random.shuffle(files)
+        total_cnt = 0
+        batch_cnt = 0
+        for f in files:
+            X[batch_cnt, ...] = np.array(Image.open(f))
+            Y[batch_cnt, ...] = float('female' in f)
+            batch_cnt += 1
+
+            if batch_cnt >= batch_size:
+                yield X, Y
+                total_cnt += batch_cnt
+                batch_cnt = 0
+                        
+
+        if batch_cnt > 0:
+            yield X[:batch_cnt, ...], Y[:batch_cnt, ...]
+            total_cnt += batch_cnt
+        epoch_sizes.append(total_cnt)
+
+        if print_stats:
+            print("Total samples cnt: {}, execution time: {}".format(total_cnt, time.time() - start_ts))
+
+        if one_epoch:
+            break
+
+def basic_cnn_model_v0(image_shape, metrics=['accuracy']):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=image_shape, name = "conv1"))
+    model.add(Conv2D(64, (3, 3), activation='relu', name = "conv2"))
+    model.add(MaxPooling2D(pool_size=(2, 2), name = "maxpool1"))
+    model.add(Dropout(0.25, name = "dropout1"))
+    model.add(Flatten(name = "flatten1"))
+    model.add(Dense(128, activation='relu', name = "dense1"))
+    model.add(Dropout(0.5, name = "dropout2"))
+    model.add(Dense(1, activation='sigmoid', name = "softmax1"))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=metrics)
+    return model
+
+def basic_cnn_model_v1(image_shape, metrics=['accuracy']):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=image_shape, name = "conv1"))
+    model.add(Conv2D(64, (3, 3), activation='relu', name = "conv2"))
+    model.add(MaxPooling2D(pool_size=(2, 2), name = "maxpool1"))
+    model.add(Dropout(0.25, name = "dropout1"))
+    model.add(Conv2D(64, (3, 3), activation='relu', name = "conv3"))
+    model.add(MaxPooling2D(pool_size=(2, 2), name = "maxpool2"))
+    model.add(Dropout(0.25, name = "dropout2"))
+    model.add(Flatten(name = "flatten1"))
+    model.add(Dense(128, activation='relu', name = "dense1"))
+    model.add(Dropout(0.5, name = "dropout3"))
+    model.add(Dense(128, activation='relu', name = "dense2"))
+    model.add(Dropout(0.5, name = "dropout4"))
+    model.add(Dense(1, activation='sigmoid', naxme = "softmax1"))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=metrics)
+    return model
+
+filenames = [];
+
+rootdir = './male/'
+for root, subFolders, files in os.walk(rootdir):
+    for file in files:
+        filenames.append(rootdir+'/'+file)
+
+rootdir = './female/'
+for root, subFolders, files in os.walk(rootdir):
+    for file in files:
+        filenames.append(rootdir+'/'+file)
+
+model = basic_cnn_model_v0((250,250,3))
+
+fg = fit_generator(filenames, 2, (250,250,3))
+
+history = model.fit_generator(fg
+                              , steps_per_epoch=10
+                              , epochs=10)
---
+++ gender.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from os.path import join, exists
+from django.http import request
+from os import rename
+import requests
+import shutil
+
+__author__ = 'Philip Masek'
+
+
+
+def main(argv):
+    fileList = []
+    fileSize = 0
+    folderCount = 0
+    rootdir = "./lfw"
+    maleFolder = "./result/male"
+    femaleFolder = "./result/female"
+    count = 0
+    tmp = ""
+
+    for root, subFolders, files in os.walk(rootdir):
+        for file in files:
+            f = os.path.join(root,file)
+            fileSize = fileSize + os.path.getsize(f)
+            fileSplit = file.split("_")
+            fileList.append(f)
+            count += 1
+
+            file_exists = exists("%s/%s" % (maleFolder,file)) or exists("%s/%s" % (maleFolder,file))
+
+            if(not file_exists):
+
+                if count == 1:
+                    result = requests.get("https://api.genderize.io/?name=%s" % fileSplit[0])
+                    try:
+                        result = result.json()
+		    except:
+         	        break;
+                    tmp = fileSplit[0]
+                elif tmp != fileSplit[0]:
+                    result = requests.get("https://api.genderize.io/?name=%s" % fileSplit[0])
+                    try:
+                        result = result.json()
+		    except:
+         	        break;
+                    tmp = fileSplit[0]
+                else:
+                    tmp = fileSplit[0]
+
+                try:
+                    print result;
+                    if float(result['probability']) > 0.9:
+                        if result['gender'] == 'male':
+                            shutil.copyfile(f,"%s/%s" % (maleFolder,file))
+                        elif result['gender'] == 'female':
+                            shutil.copyfile(f,"%s/%s" % (femaleFolder,file))
+                except Exception as e:
+                    break;
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
