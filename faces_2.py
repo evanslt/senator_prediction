2a1afa90870c3f2cecc43202ecd11e3d20a344b4
ariellel118 <ariellelittle@berkeley.edu>
2017-08-02 10:09:57 -0700

    updating faces_2 and faces_predict. Added vis

--- faces_2.py
+++ faces_2.py
@@ -14,6 +14,10 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from PIL import Image
 
+def add_noise(X, noise_ratio = 0.07):
+    X_rnd = X + np.random.random(X.shape)*noise_ratio
+    X_rnd[X_rnd>1] = 1
+    return X_rnd
 
 def compile_training_set(files, input_shape):
     num_train = len(files)
@@ -31,7 +35,9 @@ def compile_training_set(files, input_shape):
     return X, Y
 
 
-def fit_generator(files, batch_size, input_shape, print_stats=True,
+
+def fit_generator(files, batch_size, input_shape, noise, 
+    print_stats=True, 
                   one_epoch=False, epoch_sizes=[]):
 
     X = np.zeros((batch_size,) + input_shape)
@@ -45,10 +51,15 @@ def fit_generator(files, batch_size, input_shape, print_stats=True,
             image = np.array(Image.open(f))
             #image = (image - np.mean(image)) / np.var(image) # normalizing/standardizing
             image = (image - np.mean(image, axis = (0, 1), keepdims = True))/(np.var(image, axis = (0, 1), keepdims = True))
+            
+            if noise == 1:
+                image=add_noise(image)
 
             X[batch_cnt, ...] = image
             Y[batch_cnt, ...] = float('female' in f)
             batch_cnt += 1
+
+
 
             if batch_cnt >= batch_size:
                 yield X, Y
@@ -73,7 +84,10 @@ def basic_cnn_model_v0(image_shape, metrics=['accuracy']):
                      activation='relu',
                      input_shape=image_shape, name = "conv1"))
     model.add(Conv2D(64, (3, 3), activation='relu', name = "conv2"))
+
     model.add(MaxPooling2D(pool_size=(2, 2), name = "maxpool1"))
+    #model.add(Conv2D(64, (3, 3), activation='relu', name = "conv3"))
+    #model.add(MaxPooling2D(pool_size=(2, 2), name = "maxpool2"))
     model.add(Dropout(0.25, name = "dropout1"))
     model.add(Flatten(name = "flatten1"))
     model.add(Dense(128, activation='relu', name = "dense1"))
@@ -91,6 +105,28 @@ def basic_cnn_model_v1(image_shape, metrics=['accuracy']):
                      activation='relu',
                      input_shape=image_shape, name = "conv1"))
     model.add(Conv2D(64, (3, 3), activation='relu', name = "conv2"))
+    model.add(Dropout(0.25, name = "dropout1"))
+    model.add(MaxPooling2D(pool_size=(2, 2), name = "maxpool1"))
+    model.add(Dropout(0.25, name = "dropout2"))
+    model.add(Flatten(name = "flatten1"))
+    model.add(Dense(128, activation='relu', name = "dense1"))
+    model.add(Dropout(0.5, name = "dropout3"))
+    model.add(Dense(128, activation='relu', name = "dense2"))
+    model.add(Dropout(0.5, name = "dropout4"))
+    model.add(Dense(1, activation='sigmoid', name = "softmax1"))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=metrics)
+    return model
+
+
+def basic_cnn_model_v2(image_shape, metrics=['accuracy']):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=image_shape, name = "conv1"))
+    model.add(Conv2D(64, (3, 3), activation='relu', name = "conv2"))
     model.add(MaxPooling2D(pool_size=(2, 2), name = "maxpool1"))
     model.add(Dropout(0.25, name = "dropout1"))
     model.add(Conv2D(64, (3, 3), activation='relu', name = "conv3"))
@@ -101,7 +137,7 @@ def basic_cnn_model_v1(image_shape, metrics=['accuracy']):
     model.add(Dropout(0.5, name = "dropout3"))
     model.add(Dense(128, activation='relu', name = "dense2"))
     model.add(Dropout(0.5, name = "dropout4"))
-    model.add(Dense(1, activation='sigmoid', naxme = "softmax1"))
+    model.add(Dense(n_classes, activation='softmax', name = "softmax1"))
 
     model.compile(loss='binary_crossentropy',
                   optimizer='adam',
@@ -114,42 +150,45 @@ filenames_test = [];
 cnt_male = 0;
 cnt_female = 0;
 
-rootdir = '/Images/Images/male'
+rootdir = '/Images/male'
 for root, subFolders, files in os.walk(rootdir):
     for file in files:
         cnt_male += 1;
-        if cnt_male < 1280:
+        if cnt_male < 5150:
             filenames_train.append(rootdir + '/' + file)
-        elif cnt_male<1285:
+        elif cnt_male<5155:
             filenames_test.append(rootdir + '/' + file)
 
-rootdir = '/Images/Images/female'
+rootdir = '/Images/female'
 for root, subFolders, files in os.walk(rootdir):
     for file in files:
         cnt_female += 1;
-        if cnt_female < 1280:
+        if cnt_female < 1250:
             filenames_train.append(rootdir + '/' + file)
-        elif cnt_female<1285:
+        elif cnt_female<1360:
             filenames_test.append(rootdir + '/' + file)
 
 
+# currently doing 208 total for training
+# remember to transfer the test set to the predict script
+
+# atch 64 steps_per_epoch 20, one will be missing 
 
 X, Y = compile_training_set(filenames_test, (250, 250, 3))
 
 model = basic_cnn_model_v0((250,250,3))
 
 
-fg = fit_generator(filenames_train, 64, (250, 250, 3))
+fg = fit_generator(filenames_train, 64, (250, 250, 3), noise=1)
 
 history = model.fit_generator(fg
-                              , steps_per_epoch=40
-                              , epochs=30)
+                              , steps_per_epoch=100
+                              , epochs=50)
 
-model.save("/output/face_model_2520_b64_e30.h5")
+model.save("/output/face_model_6400_b64_e50-reorder-v0-noise.h5")
 
 prediction_probabilities = model.predict(X)[:,0]
 print(filenames_test)
 print(prediction_probabilities)
 print(Y)
-
 #
--- predict_faces.py
+++ predict_faces.py
@@ -13,34 +13,41 @@ from keras.layers import MaxPooling2D
 from PIL import Image
 from keras.models import load_model
 
-model = load_model("/out/face_model_2520_b64_e30.h5")
+model = load_model("/out/face_model_6400_b64_e25_s100-reorder-v1.h5")
 
-filenames = [];
+filenames_validation = [];
+filenames_test = [];
+
 
 cnt_male   = 0;
 cnt_female = 0;
 
 # Currently predicting for first 10 images in male and female
 
-rootdir = '/Images/Images/male/'
+rootdir = '/Images/male/'
 for root, subFolders, files in os.walk(rootdir):
     for file in files:
         cnt_male += 1;
-        if cnt_male > 1285:
-            filenames.append(rootdir+'/'+file)
-            print(file)
-        if cnt_male > 1300:
-            break;
+        #if cnt_male > 3870:
+        if cnt_male > 0 and cnt_male<500:
+            filenames_validation.append(rootdir+'/'+file)
+            #print(file)
+        if cnt_male > 5150:
+            filenames_test.append(rootdir+'/'+file)
+            #break;
 
-rootdir = '/Images/Images/female/'
+rootdir = '/Images/female/'
 for root, subFolders, files in os.walk(rootdir):
     for file in files:
         cnt_female += 1;
-        if cnt_female >1285:
-            filenames.append(rootdir+'/'+file)
-            print(file)
-        if cnt_female > 1300:
-            break;
+        #if cnt_female >1250:
+        if cnt_female >0 and cnt_female<500:
+            filenames_validation.append(rootdir+'/'+file)
+            #print(file)
+        if cnt_male > 1250:
+            filenames_test.append(rootdir+'/'+file)
+            #break;
+
 
 def compile_training_set(files, input_shape):
     num_train = len(files)
@@ -54,37 +61,64 @@ def compile_training_set(files, input_shape):
         image = (image - np.mean(image, axis = (0, 1), keepdims = True))/(np.var(image, axis = (0, 1), keepdims = True))
         X[idx, ...] = image # expand the dimension since keras expects a color channel
         Y[idx, ...] = float('female' in f)
-
     return X, Y
+
 # calculate accuracy on training set
-X, Y = compile_training_set(filenames, (250,250,3))
+X, Y = compile_training_set(filenames_validation, (250,250,3))
+X_test, Y_test =compile_training_set(filenames_test, (250, 250, 3))
 
-print('should be 1 if female, 0 if male')
-print("predictions", model.predict(X)[:, 0])
-print("actual", Y)
+pred_val=model.predict(X)[:, 0]
 
-pred=model.predict(X)[:, 0]
-scores=[]
-filenames_short=[]
-for file in filenames:
-    file = file[20::]
-    filenames_short.append(file)
+pred_test=model.predict(X_test)[:, 0]
 
-for i, val in enumerate(pred):
+scores_val=[]
+scores_test=[]
+filenames_v_short=[]
+filenames_t_short=[]
+
+for file in filenames_validation:
+    file = file[13::]
+    filenames_v_short.append(file)
+
+for file in filenames_test:
+    file = file[13::]
+    filenames_t_short.append(file)
+
+for i, val in enumerate(pred_val):
     diff=val-Y[i]
     if np.around(diff, decimals=1) == 0:
-        scores.append(1)
+        scores_val.append(1)
     else:
-        scores.append(0)
-print("number correct:", np.sum(scores))
+        scores_val.append(0)
 
-#file=open('/output/prediction_0731_test_230img-2.dat','wb')
-#data = np.zeros(np.size(filenames),dtype=[('1','S20'),('2',float), ('3', float)])
-#data['1']=filenames_short
-#data['2']=model.predict(X)[:, 0]
-#data['3']=Y
-#np.savetxt(file, data, fmt="%s %f %f")
-#file.close()
+for i, val in enumerate(pred_test):
+    diff=val-Y_test[i]
+    if np.around(diff, decimals=1) == 0:
+        scores_test.append(1)
+    else:
+        scores_test.append(0)
+
+print("number correct in validation:", np.sum(scores_val))
+print("out of:", np.size(scores_val))
+print("number correct in test:", np.sum(scores_test))
+print("out of:", np.size(scores_test))
+
+file=open('/output/validation-129.dat','wb')
+data = np.zeros(np.size(filenames_v_short),dtype=[('1','S20'),('2',float), ('3', float)])
+data['1']=filenames_v_short
+data['2']=pred_val
+data['3']=Y
+np.savetxt(file, data, fmt="%s %f %f")
+file.close()
+
+
+file=open('/output/test-129.dat','wb')
+data = np.zeros(np.size(filenames_t_short),dtype=[('1','S20'),('2',float), ('3', float)])
+data['1']=filenames_t_short
+data['2']=pred_test
+data['3']=Y_test
+np.savetxt(file, data, fmt="%s %f %f")
+file.close()
 # name = "Brad_Wilk_0001"
 
 # this currently predicts for one image
